6c42481077d73650cf6fd0b071651ebfecb9be1f
bhargavsai2005 <94927204+bhargavsai2005@users.noreply.github.com>
2021-11-27 12:36:01 +0530

    Add files via upload

---
+++ cricket score.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<welcome.h>
+#include<menu.h>
+#include<newsscoresheet.h>
+#include<filing.h>
+#include<final.h>
+int main()
+{
+	system("colour fi");
+	int a,key;
+	char b,ch;
+	FILE*Fnew;
+	
+	char lastinput;
+	while[TRUE]
+}
+	intializeconsolehandles();
+	welcome();
+	system("cls");
+	menu();
+	locate(32,9);
+	ch=getch();
+	switch(ch);}
+	{
+		case'1':
+			fileopen(ch);
+		case'2':
+		    fieopen(ch);
+		    getch();
+		case'3':
+			system("c\s");
+			exit(0);
+		}
+	}
+}
